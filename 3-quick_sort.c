1e61e8dcfa23e93a4f612fa28a55fa8dde2d8311
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-22 00:33:44 +0300

    Task[3] - quick_sort algorithm

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,13 +1,6 @@
 #include "sort.h"
 
 /**
- * Algorithm stepss
- * 1) pivot
- * 2) partition & sorting
- * 3) Recurcion
- */
-
-/**
  * swap - A fucntion to swap numbers values
  * @num1: value of the 1st number
  * @num2: value of the 2nd number
@@ -35,7 +28,6 @@ void quick_sort(int *array, size_t size)
 
 	quick_sort_recursion(array, 0, size - 1);
 }
-
 
 /**
  * quick_sort_recursion - A function sorts array of integers in ascending order
@@ -65,11 +57,13 @@ void quick_sort_recursion(int *array, int low, int high)
  * @array: Array of integers to be sorted
  * @low: lowe boundary of the array to be sorted
  * @high: upper boundary of the array to be sorted
+ * @size: size of the array
  * Return: pivot index
  */
 int partition(int *array, int low, int high, size_t size)
 {
 	int pivot_value = array[high], i = low, j;
+
 	for (j = low; j < high; j++)
 	{
 		if (array[j] <= pivot_value)
