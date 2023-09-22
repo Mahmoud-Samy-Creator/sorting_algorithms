69847ee964a0c7539cc0a6c80f8bb7d4b592dc22
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-09-22 12:38:42 +0300

    update prototypes

--- sort.h
+++ sort.h
@@ -35,12 +35,11 @@ void selection_sort(int *array, size_t size);
 
 /* Quick sort functions */
 void quick_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
-void quick_sort_recursion(int *array, int low, int high);
+void quick_sort_recursion(int *array, int low, int high, size_t size);
 int partition(int *array, int low, int high, size_t size);
 
 
-/* => Advance <=*/
+/* => Advanced <=*/
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
